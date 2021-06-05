5cc8d8a8416605609dc1e843dbdc6e1610e57683
patelashok <ashok.patel2917@gmail.com>
2021-06-05 14:57:52 +0530

    Adding new contact

---
+++ AddressBook.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddressBook {
+
+    static Scanner sc = new Scanner(System.in);
+    static List<Contact> list = new ArrayList<>();
+
+    //Created method for adding contact
+    public static void addContact()
+    {
+        System.out.println("Enter your firstName : ");
+        String firstName = sc.nextLine();
+        System.out.println("Enter your lastName : ");
+        String lastName = sc.nextLine();
+        System.out.println("Enter your address : ");
+        String address = sc.nextLine();
+        System.out.println("Enter your city : ");
+        String city = sc.nextLine();
+        System.out.println("Enter your state : ");
+        String state = sc.nextLine();
+        System.out.println("Enter your zipCode : ");
+        String zip = sc.nextLine();
+        System.out.println("Enter your phoneNo : ");
+        String phoneNo = sc.nextLine();
+        System.out.println("Enter your emailId : ");
+        String email = sc.nextLine();
+        Contact obj = new Contact(firstName, lastName, address, city, state, zip, phoneNo, email);
+        list.add(obj);
+    }
+
+
+    public static void main(String[] args) {
+
+        AddressBook addressBook = new AddressBook();
+        //Displaying the welcome message
+        System.out.println("WELCOME TO ADDRESS BOOK PROBLEM");
+        //Adding new contact
+        System.out.println("Enter details of new contact");
+        addContact();
+
+        //Creating contact and adding new contact details to the list
+        System.out.println("You want to add contact Y/N");
+        String chose = sc.nextLine();
+        if(chose.equalsIgnoreCase("y"))
+            addContact();
+        else
+            System.out.println(list); //printing list
+    }
+}
